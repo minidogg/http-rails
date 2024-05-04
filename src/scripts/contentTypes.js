1c360258ca1083bbb22cfd19d3d45ce78281c3eb
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-05-04 14:49:14 -0500

    added some more content types

--- src/scripts/contentTypes.js
+++ src/scripts/contentTypes.js
@@ -1,9 +1,24 @@
 module.exports = {
-    //text
+    //?text
     css: "text/css",
     csv: "text/csv",
     html: "text/html",
     js: "text/javascript",
     txt: "text/plain",
     xml: "text/xml",
+    //?application
+    jar:"application/java-archive",
+    bin:"application/octet-stream",
+    ogg:"application/ogg",
+    pdf:"application/pdf",
+    xtml:"application/xhtml+xml",
+    json:"application/json",
+    jsonld:"application/ld+json",
+    // xml:"application/xml",
+    zip:"application/zip",
+    //?audio
+    mp3:"audio/mpeg",
+    //?video
+    mp4:"video/mp4", 
+    webm:"video/webm",
   };
